1a7bc2f690c683f3556e0064027eb8aa067e3d40
lidanxie <987905457@qq.com>
2017-08-15 19:20:32 +0800

    nodejs发送邮件

---
+++ LearnNode/nodemailer/app.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+
+//开启SMTP连接池
+let transporter = 
+
+https://nodemailer.com/about/
+http://www.lovebxm.com/2017/07/21/node-mail/
+https://segmentfault.com/a/1190000010291860
+
+
+/** superagent */
+http://www.jianshu.com/p/74232649e228
+http://www.jianshu.com/p/98b854322260
+https://cnodejs.org/topic/5378720ed6e2d16149fa16bd
